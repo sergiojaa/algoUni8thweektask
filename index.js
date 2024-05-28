b87e1a85732b1e36b7f21a813beebe6356047711
seriojaa <119433399+sergiojaa@users.noreply.github.com>
2024-05-28 21:47:36 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,140 +1,122 @@
 //1. დაწერეთ ფუნქცია, რომელიც პარამეტრად იღებს callback ფუქნციას და იძხებს მას 1 წამის შემდეგ.
-// setTimeout(()=> console.log('hello async programming'),1000)
+// setTimeout(()=> console.log('get in 1 second'),1000 )
+// function getInSecond(callback){
+//     setTimeout(()=> console.log(callback),1000)
+// }
+// getInSecond('hi bro')
 
 //2. დაწერეთ ფუქნცია, რომელიც არგუმენტად მიიღებს ორ რიცხვს და ორ ქოლბექ ფუქნციას. თუ პირველი რიცხვი მეტია მეორეზე, 
 //გამოიძახეთ პირველი ქოლბექი, თუარადა გამოიძხეთ მეორე ქოლბექ ფუნქცია.
-
-// function checkNumber(num1, num2, funcEquals, funcNotEquals){
+// function getElements(num1, num2, callback1, callback2){
 //     if(num1 > num2){
-//         funcEquals()
+//        return callback1()
 //     }else{
-//         funcNotEquals()
+//         return callback2()
 //     }
 // }
-// checkNumber(
-//     10,
-//     20,
-//     () => {
-//         console.log('func not equals')
-//     },
-//     ()=> {
-//         console.log('func  equals')
-//     }
-
-// )
-
+// const number1 = Number(prompt('enter the first number'))
+// const number2 = Number(prompt('enter the second number'))
+// getElements( 
+//     number1,
+//     number2,
+//     ()=> console.log(`${number1} მეტია ${number2}-ზე ${number1 - number2}-ით`),
+//     ()=> console.log(`${number1} ნაკლებია ${number2}-ზე ${number1 - number2}-ით`)
+//  )
 
 //3. დაწერეთ ფუნქცია, რომელიც პარამეტრებად იღებს მასივს, რიცხვს და callback ფუქნციას. callback ფუნქციის გამოძახების
 //შემდეგ მასივში უნდა ჩაიფუშოს რიცხვი და დაბრუნოს განახლებული მასივი. მინიშნება: callback ფუნქციას გამოძახებისას არგუმენტებად
 // გაატანეთ მასივი და რიცხვი, რომელიც მასივში უნდა ჩაიფუშოს.
-// function addNumber(arr, number, callback){
-//     callback(arr,number)
+// function getParams(arr, num, callback){
+//     callback(arr, num)
 //     return arr
 // }
-
-// function addToArray(arr,number){
-//     arr.push(number)
+// let array = [10,20,30,40]
+// const number = 50
+// function addToArray(arr,num){
+//     arr.push(num)
+//     return arr
 // }
-// let myArray = [1,2,3,4]
-// let num = 5
-// let updatedArray = addNumber(myArray,num,addToArray)
-// console.log(updatedArray)
-
+// console.log(getParams(array, number, addToArray))
 
 //4. შექმენით ფრომისი, რომელიც დარეზოლვდება 2 წამის შემდეგ და გამოიტანს წარამტების შეტყობინებას.
-// const promise = new Promise((resolve) => {
-//     setTimeout(()=> resolve('success'),3000)
+// const promise = new Promise(resolve => {
+//     setTimeout(resolve('success'),2000)
 // })
 // promise.then(message => console.log(message))
-
 //5. შექმენით ფრომისი, რომელიც დარეზოლვდება ან დარეჯექთდება 3 წამის შემდეგ. დარეზოლვდეს თუ იუზერი სრულწლოვანია და დარეჯექთდეს - თუ არაა.
-
-// let user = 19;
+// let user1 = Number(prompt('enter your age'))
 // const promise = new Promise((resolve, reject)=>{
-//     setTimeout(()=>{
-//         if(user >= 18){
-//             resolve('srulwlovani')
+//     setTimeout( ()=>{
+//         if(user1 >= 18){
+//             resolve('adult')
 //         }else{
-//             reject('araswulwlovani')
+//             reject('minor')
 //         }
-//     },3000)
+//     } )
 // })
-// promise.then(message => {
-//     console.log(message)
-// }).catch(error => console.log(error))
+// promise.then(message=> console.log(message)).catch(error => console.log(error))
 
 //6. შექმენით ფრომისი, რომელიც დაარეზოლვებს შემთხვევების 50%-ში და დანარჩენ 50%-ში დაარეჯექთებს. დაუმკლავდით ორივე ვარიანტს then და catch-ით.
-// const promise = new Promise((resolve, reject) => {
+// const promise = new Promise((resolve, reject)=> {
 //     if(Math.random() > 0.5){
-//         resolve('resolved metia')
+//         resolve('metia')
 //     }else{
-//         reject('rejected naklebia')
+//         reject('naklebia')
 //     }
 // })
-// promise.then(result => {
-//     console.log(result)
-// }).catch(error=>{
-//     console.log(error)
-// })
-
-
+// promise.then(message=>console.log(message)).catch(error=>console.log(error))
 
 //7. შექმენით Promise Chain (მინიმუმ 3 ფრომისით), სადაც თითოეული დარეზოლვდება 1 წამში და გამოიტანს განსხვავებულ წარმატების შეტყობინებებს.
 // const promise = new Promise((resolve)=>{
-//     setTimeout((resolve('resolved')),1000)
-//     return resolve
+//     setTimeout(resolve('success'),1000)
 // })
-// promise.then(resolve=>{
-//     console.log(resolve)
-//     return resolve
-// }).then(resolve=>{
-//     console.log(resolve)
-//     return resolve
-// }).then(resolve => {
-//     console.log(resolve)
-//     return resolve
-// }).catch(error=> console.log(error))
+// promise.then(response=> {
+//     console.log(response)
+//     return response
+// }).then(response=>{
+//     console.log(response)
+//     return response
+// } ).then(response=> {
+//     console.log(response)
+//     return response
+// }).catch(error=>console.log(error))
+
 
 //8. შექმენით ორი ფრომისი. პირველმა დაარეზოლვოს რიცხვი და მეორემ დარეზოლვებული რიცხვი გაამრავლოს 2-ზე. გამოიყენეთ promise chaining.
-// const firstPromise = new Promise((resolve)=>{
-//     resolve(17)
+// const promise1 = new Promise((resolve)=>{
+//     resolve(20)
 // })
-// firstPromise.then((number)=>{
-//     console.log("first promise" + number)
+// promise1.then((number)=>{
+//     console.log('first number' + " " + number)
 //     return number
 // }).then((number)=>{
 //     const num2 = number * 2
-//     console.log('second promise' + num2)
+//     console.log('second number' + " " + num2)
 //     return num2
-// }).catch((error)=>{
-//     console.log(error)
 // })
 //9. დაწერეთ პროგრამა, რომელიც მომხმარებელს შეეკითხება დადებით რიცხვს და დაითვლის მაგ რიცხვამდე. თითოეული რიცხვი უნდა გამოკონსოლდეს
 // 1 წამის ინტერვალით.
-// const num = 5
+// const userNumber = 5
 // let currentNumber = 1
-// const interval = setInterval(()=>{
+// const interval1 = setInterval(()=>{
 //     console.log(currentNumber)
-//     if(currentNumber === num){
-//         clearInterval(interval)
+//     if(currentNumber === userNumber){
+//         clearInterval(interval1)
 //     }
 //     currentNumber++
 // })
 
-
-
 //10. დაწერეთ პროგრამა, რომელიც მომხმარბელს შეეკითხება თავისი გითჰაბის იუზერნეიმს და წამოიღებს მის ინფორმაციას გითჰაბის API-დან.
 // API ლინკი - https://api.github.com/users/[username], მაგალითად: https://api.github.com/users/temuritsutskiridze. 
 //ჯერ გამოიყენეთ then/catch და შემდეგ async/await.
-// https://api.github.com/users/sergiojaa
-// async function getUser(username){
-//     const response = await fetch(`https://api.github.com/users/${username}`)
+// const githubUser = prompt('enter the github username')
+// async function githubUrl(name){
+//     const response = await fetch(`https://api.github.com/users/${name}`)
 //     const data = await response.json()
 //     console.log(data)
 // }
-// getUser('sergiojaa')
-
-//11. დაწერეთ პროგრამა, რომელსაც API-დან მოაქვს ორი განსხვავებული ინფორმაცია და დაბეჭდოს, როდესაც ორივეს მიიღებს
+// githubUrl(githubUser)
+// 11. დაწერეთ პროგრამა, რომელსაც API-დან მოაქვს ორი განსხვავებული ინფორმაცია და დაბეჭდოს, როდესაც ორივეს მიიღებს
 // const fetchData1 = fetch("https://jsonplaceholder.typicode.com/todos/1")
 // const fetchData2 = fetch("https://jsonplaceholder.typicode.com/todos/2")
 // Promise.all([fetchData1,fetchData2]).then(responses => {
@@ -149,5 +131,11 @@
 //     "https://jsonplaceholder.typicode.com/todos/2",
 //     "https://jsonplaceholder.typicode.com/todos/3"
 // ]
-// const promises = urls.map( url => fetch(url).then(response => response.json()) )
-// Promise.race(promises).then((fastestData) => console.log(fastestData) )
+// // unda shevqmnat cvladi gadavurbinot mapit davfetchot da gamovitanot responsit
+// const promises = urls.map(url=> fetch(url).then(response=>response.json()) )
+// Promise.race(promises).then(fastestData=>console.log(fastestData))
+// Promise.race(promises).then(fastestData => console.log(fastestData)) 
+
+// const promises = urls.map(url => fetch(url).then(response => response.json()));
+// Promise.race(promises).then(fastestData => console.log(fastestData));
+
